6e8152a2305da366c1559a27de3db02b74862f72
Lynn Sakashita <dev.lynn.sakashita@gmail.com>
2023-02-21 19:26:02 +0900

    Fix `FactService`

--- app/src/main/java/jp/speakbuddy/edisonpractice/network/FactService.kt
+++ app/src/main/java/jp/speakbuddy/edisonpractice/network/FactService.kt
@@ -1,26 +1,27 @@
 package jp.speakbuddy.edisonpractice.network
 
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.Retrofit
 import retrofit2.http.GET
 
 interface FactService {
-
-    @GET("/fact")
+    @GET("fact")
     suspend fun getFact(): FactResponse
 }
 
+@Serializable
 data class FactResponse(
     val fact: String,
     val length: Int
 )
 
-object RetrofitProvider {
+object FactServiceProvider {
     fun provide(): FactService =
         Retrofit.Builder()
-            .baseUrl("https://example.com/")
+            .baseUrl("https://catfact.ninja/")
             .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
             .build()
             .create(FactService::class.java)
